5a981d2328cdf511e7db2600f9ee4d18ec2cb50a
Justin Lohner <lohner4@gmail.com>
2020-03-23 13:03:51 -0600

    got the makePersonObject function working

--- index.js
+++ index.js
@@ -100,8 +100,13 @@ function temperatureInF(temp, unit) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(id, name, email) {
+  const person = {
+	id: id,
+	name: name,
+	email: email
+  }
+  return person;
 }
 
 /**
